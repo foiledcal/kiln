cff7a502c5dcf60e0dd8977ad6cd921bc8711210
foiledcal <foiledcal@proton.me>
2025-10-07 03:11:10 +0000

    0.0.20

--- littleTest.py
+++ littleTest.py
@@ -3,20 +3,13 @@ import busio
 import digitalio
 import time
 
-import adafruit_max31855
-
-
-spi = board.SPI()
-cs = digitalio.DigitalInOut(board.D5)               #GPIO5
-max31855 = adafruit_max31855.MAX31855(spi, cs)
-
 #soft off switch: 1 if open, 0 if closed
-offSwitch = digitalio.DigitalInOut(board.D12)
+offSwitch = digitalio.DigitalInOut(board.D18)
 offSwitch.direction = digitalio.Direction.INPUT
-offSwitch.pull = digitalio.Pull.DOWN
+offSwitch.pull = digitalio.Pull.UP
 
 while True:
-    if offSwitch.pull:
+    if offSwitch.value:
         print("Switch is on")
     else:
         print("switch is off")
