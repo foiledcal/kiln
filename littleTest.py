d66968b83c04908675f55c4a447fe0549f8c5dc8
foiledcal <foiledcal@proton.me>
2025-12-10 05:57:12 +0000

    xc

--- littleTest.py
+++ littleTest.py
@@ -13,5 +13,5 @@ relay1.direction = digitalio.Direction.OUTPUT
 relay2 = digitalio.DigitalInOut(board.D27)          
 relay2.direction = digitalio.Direction.OUTPUT
 
-relay1.value = 1
-relay2.value = 1
+relay1.value = 0
+relay2.value = 0
