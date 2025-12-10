32067d04954877b5b66ba5d5b56818b490d17276
foiledcal <foiledcal@proton.me>
2025-12-10 06:29:53 +0000

    0.0.36

--- littleTest.py
+++ littleTest.py
@@ -6,9 +6,9 @@ import time
 import adafruit_max31855
 
 #relays
-relay1 = digitalio.DigitalInOut(board.D22)          
+relay1 = digitalio.DigitalInOut(board.D24)          
 relay1.direction = digitalio.Direction.OUTPUT
-relay2 = digitalio.DigitalInOut(board.D27)          
+relay2 = digitalio.DigitalInOut(board.D23)          
 relay2.direction = digitalio.Direction.OUTPUT
 
 #soft off switch: off is TRUE, on is FALSE
@@ -18,11 +18,11 @@ offSwitch.pull = digitalio.Pull.UP
 
 while True:
     print(offSwitch.value)
-    if not offSwitch.value:
+    if offSwitch.value:
         relay1.value = 0
         relay2.value = 0
-        time.sleep(0.1)
+        time.sleep(0.5)
     else:
         relay1.value = 1
         relay2.value = 1
-        time.sleep(0.1)
+        time.sleep(0.5)
